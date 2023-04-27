391abe527181cfb641462cfc9076a951e4a8738a
xiaoyicong <xiaoyicongedward@gmail.com>
2023-04-27 23:52:50 +0800

    commit

--- src/ResultProcessor/HITS.java
+++ src/ResultProcessor/HITS.java
@@ -1,5 +1,6 @@
 package ResultProcessor;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -43,23 +44,50 @@ public class HITS {
         }
     }
 
-    public void rank(double threshold){
+    public HashMap<String,Double> rank(double threshold) throws IOException {
         double delta=100;
         while(delta>=threshold){
+            double authSum = 0, oldAuthSum=0, hubSum=0, oldHubSum = 0;
             for (String docID: subgraph){
+                oldAuthSum+=Authority.get(docID);
                 updateAuth(docID);
+                authSum+=Authority.get(docID);
+            }
+            for (String docID: subgraph){
+                Authority.put(docID,Authority.get(docID)/authSum);
+                oldHubSum+=Hub.get(docID);
+                updateHub(docID);
+                hubSum+=Hub.get(docID);
             }
             for(String docID:subgraph){
-                updateHub(docID);
+                Hub.put(docID,Hub.get(docID)/hubSum);
             }
+
+            delta = Math.abs(hubSum-oldHubSum)+Math.abs(authSum-oldAuthSum);
         }
+
+        return Authority;
     }
-    private void updateAuth(String docID){
+    private void updateAuth(String docID) throws IOException {
 
+        HTree child2parent = fileManager.getIndexFile(FileNameGenerator.getWebGraphName_child2parent(docID)).getFile();
+        ArrayList<String> parents = (ArrayList<String>)child2parent.get(docID);
+
+        double newAuth = 0.0;
+        for(String parent:parents){
+            newAuth+=Hub.get(parent);
+        }
+        Authority.put(docID,newAuth);
     }
 
-    private void updateHub(String docID){
-
+    private void updateHub(String docID) throws IOException {
+        HTree parent2child = fileManager.getIndexFile(FileNameGenerator.getWebGraphName_parent2child(docID)).getFile();
+        ArrayList<String> children = (ArrayList<String>)parent2child.get(docID);
+        double newHub = 0.0;
+        for(String child:children){
+            newHub+=Authority.get(child);
+        }
+        Hub.put(docID,newHub);
     }
 
 
